c5998ba039d6509a68717846b0e389f3a8fbd908
Cleiton Braga <cleitonbraga56@gmail.com>
2023-06-05 23:39:35 -0300

    Add test 'Inserir registro no arquivo de log'

--- src/problema.js
+++ src/problema.js
@@ -5,17 +5,83 @@ class EntradasNoBranco {
     LOG_FOLDER = `${__dirname}/log/`;
     LOG_FILE_PATH = `${this.LOG_FOLDER}log.txt`;
 
+
+    padTo2Digits(num) {
+        return num.toString().padStart(2, '0');
+    }
+
+    formatDate(date) {
+        return (
+            [
+                date.getFullYear(),
+                this.padTo2Digits(date.getMonth() + 1),
+                this.padTo2Digits(date.getDate()),
+            ].join('-') +
+            ' ' +
+            [
+                this.padTo2Digits(date.getHours()),
+                this.padTo2Digits(date.getMinutes()),
+                this.padTo2Digits(date.getSeconds()),
+            ].join(':')
+        );
+    }
+
     constructor() {}
 
     logFileExist() {
         return new Promise((resolve, reject) => {
             fs.access(this.LOG_FILE_PATH, fs.F_OK, (error) => {
-                if(error) {
+                if (error) {
                     console.error('Erro ao acessar arquivo: ', error);
                     resolve(false);
                 } else {
                     resolve(true);
                 }
+            });
+        });
+    }
+
+    insertRegisterAtLog(datetime) {
+        this.checkDatetimeFormat(datetime);
+        this.checkDatetimeRange(datetime);
+        this.writeAtLogFile(`${datetime} - Abertura da porta OK`);
+    }
+
+    checkDatetimeFormat(datetime) {
+        if(!datetime) throw new Error('O parâmetro (data/hora) deve ser informado para a validação do formato!');
+
+        const formated = datetime.toString().split(" ");
+        const date = formated[0];
+        const time = formated[1];
+
+        if(!date.match(/^([0-9]{4}-[0-9]{2}-[0-9]{2})$/)) throw new Error('A data é inválida!')
+
+        if(!time.match(/^([0-9]{2}:[0-9]{2}:[0-9]{2})$/)) throw new Error('A hora é inválida!');
+    }
+
+    checkDatetimeRange(datetime) {
+        if(!datetime) throw new Error('O parâmetro (data/hora) deve ser informado para a validação do horário de funcionamento!');
+
+        const date = datetime.toString().split(" ")[0];
+
+        const currentDate = new Date(datetime).getTime();
+        const firstDate = new Date(`${date} 10:00:00`).getTime();
+        const lastDate  = new Date(`${date} 16:00:00`).getTime();
+
+        if(!(currentDate >= firstDate && currentDate <= lastDate)) {
+            throw new Error('A hora de entrada está fora do expediente!');
+        }
+    }
+
+    writeAtLogFile(data) {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(this.LOG_FILE_PATH, data, (error) => {
+                if(error) {
+                    console.error('Erro ao escrever no arquivo: ', error);
+                    resolve(false);
+                }
+
+                resolve(true);
             });
         });
     }
--- src/problema.spec.js
+++ src/problema.spec.js
@@ -7,3 +7,26 @@ describe('Verificar existência do arquivo de log', () => {
         expect(await entradasNoBranco.logFileExist()).toEqual(true);
     });
 });
+
+//* Pessoa passou pela porta (Inserir no registro)
+
+//* Inserir registro no arquivo de log
+describe('Inserir registro no arquivo de log', () => {
+    it('O registro de entrada deve ser válido', () => {
+        const entradasNoBranco = new EntradasNoBranco();
+
+        expect(entradasNoBranco.checkDatetimeFormat('2023-06-05 16:00:00')).toEqual();
+    });
+
+    it('A hora deve estar entre o horário de funcionamento', () => {
+        const entradasNoBranco = new EntradasNoBranco();
+
+        expect(entradasNoBranco.checkDatetimeRange('2023-06-05 16:00:00')).toEqual();
+    });
+
+    it('Deve incluir um registro no arquivo de log com sucesso', () => {
+        const entradasNoBranco = new EntradasNoBranco();
+
+        expect(entradasNoBranco.insertRegisterAtLog('2023-06-05 10:00:00')).toEqual();
+    });
+});
